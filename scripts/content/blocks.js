605f7f628d312487e9cafa6dc18f8b2f766e5e96
Drifted Notes <109656172+drfn@users.noreply.github.com>
2022-09-05 15:32:11 +0800

    Update blocks.js

--- scripts/content/blocks.js
+++ scripts/content/blocks.js
@@ -4,14 +4,14 @@ const defense = [
 	extend(Wall, "test", {
 		health: 9999999,
 		chanceDeflect: 1,
-		requirements: ItemStack.with(binaryItems.zero, 5)),
+		requirements: ItemStack.with(binaryItems.zero, 5))
 	})
 ];
 
 for(let i = 0;i<defense.length;i++){
 	defense[i].buildVisibility = BuildVisibility.shown;
 	defense[i].category = Category.defense;
-}
+};
 
 module.exports = {
 	defense[0]: [`${defense[0].name}`]
