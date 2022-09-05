f9ce8d00713d4e186fb9e9a200bc00e02c43423a
Drifted Notes <109656172+drfn@users.noreply.github.com>
2022-09-05 15:51:18 +0800

    Update blocks.js

--- scripts/content/blocks.js
+++ scripts/content/blocks.js
@@ -1,16 +1,7 @@
-const binaryItems = require('content/items');
+var defense = [
+	"test"
+]
 
-const defense = [ 
-	extend(Wall, "the-what", {
-		health: 20000
-	})
-];
-
-for(let i = 0;i<defense.length;i++){
-	defense[i].buildVisibility = BuildVisibility.shown;
-	defense[i].category = Category.defense;
-};
-
-module.exports = {
-	defense[0]: test
-};
+for(let i in defense){
+	require("blocks/defense/"+i);
+}
