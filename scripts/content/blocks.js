67d7f209c58cd0408617741ffd852d975a7f121f
Drifted Notes <109656172+drfn@users.noreply.github.com>
2022-09-05 14:46:03 +0800

    Update blocks.js

--- scripts/content/blocks.js
+++ scripts/content/blocks.js
@@ -1,1 +1,11 @@
+const binaryItems = require('content/items');
 
+const test = extend(Wall, "test", {
+	health: 9999999,
+	chanceDeflect: 1,
+});
+test.requirements(Category.defense, ItemStack.with(binaryItems.zero, 5));
+
+module.exports = {
+	 test
+};
